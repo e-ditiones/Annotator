82bc316750b9406efe3550873f4f6852b394891f
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-08-02 13:15:42 +0200

    Delete run.sh

--- presto-tagger/run.sh
+++
@@ -1,5 +0,0 @@
-./tagger.py --predict=logs/prestov5.3-tagger \
-        --test_data=data/test-data.conll \
-        --fasttext_model=embeddings/cc.fr.300.bin \
-        --name=LSTM-CRF-TEST \
-        > data/result_test_data.conll
