a048b79b4291d1202c9e3df0d5b14a047efcd0fd
Alexandre Bartz <59565494+alexbartz@users.noreply.github.com>
2021-10-15 16:19:54 +0200

    minor changes

--- presto-tagger/prepare.sh
+++ presto-tagger/prepare.sh
@@ -8,7 +8,7 @@ mkdir data
 # Download the embeddings, the model and the data 
 wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.fr.300.bin.gz -P embeddings/
 gunzip embeddings/cc.fr.300.bin.gz
-curl https://sharedocs.huma-num.fr/wl/?id=Kq2woXBVoUv8BIyEQrIP0L0dv6XysWO3&fmode=download --output logs/prestov5.3-tagger.zip
-unzip logs/prestov5.3-tagger.zip -d logs/
-curl https://sharedocs.huma-num.fr/wl/?id=hNkFbpu7qU4uQsvRaPWM3mm8SEK5CypU&fmode=download --output data.zip
-unzip data.zip -d data/
+#curl https://sharedocs.huma-num.fr/wl/?id=Kq2woXBVoUv8BIyEQrIP0L0dv6XysWO3&fmode=download --output logs/prestov5.3-tagger.zip
+#unzip logs/prestov5.3-tagger.zip -d logs/
+#curl https://sharedocs.huma-num.fr/wl/?id=hNkFbpu7qU4uQsvRaPWM3mm8SEK5CypU&fmode=download --output data.zip
+#unzip data.zip -d data/
